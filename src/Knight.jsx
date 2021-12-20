b44783eb4333d0a2bad27755baf5dc95420cf9c3
Alex922 <gggggu@example.com>
2021-12-20 07:56:31 +0200

    first comit

---
+++ src/Knight.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ItemTypes } from './Constants'
+import { useDrag } from 'react-dnd'
+
+export default function Knight() {
+  const [{isDragging}, drag] = useDrag(() => ({
+    type: ItemTypes.KNIGHT,
+    collect: monitor => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }))
+
+  return (
+    <div
+      ref={drag}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        fontSize: 20,
+        fontWeight: 'bold',
+        cursor: 'move',
+      }}
+    >
+      ♘
+    </div>
+  )
+}
